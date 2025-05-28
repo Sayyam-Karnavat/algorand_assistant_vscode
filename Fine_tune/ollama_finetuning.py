3d93c4588ccf042ed84c1997b1f5292a072122f5
Sayyam-Karnavat <sanyam.karnavat5@gmail.com>
2025-05-28 18:25:19 +0530

    Prepared the prompt and formatted the question answer dataset.

--- Fine_tune/ollama_finetuning.py
+++ Fine_tune/ollama_finetuning.py
@@ -41,3 +41,32 @@ model = FastLanguageModel.get_peft_model(
     loftq_config = None, # And LoftQ
 )
 
+
+alpaca_prompt = """You are an algorand blockchain developers study the following question and answer. 
+Study them and remember to answer them when user asks relevant questions regarding algorand blockchain.
+
+### Instruction:
+{}
+
+### Input:
+{}
+
+### Response:
+{}"""
+
+EOS_TOKEN = tokenizer.eos_token # Must add EOS_TOKEN
+def formatting_prompts_func(examples):
+    instructions = examples["instruction"]
+    inputs       = examples["input"]
+    outputs      = examples["output"]
+    texts = []
+    for instruction, input, output in zip(instructions, inputs, outputs):
+        # Must add EOS_TOKEN, otherwise your generation will go on forever!
+        text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
+        texts.append(text)
+    return { "text" : texts, }
+pass
+
+from datasets import load_dataset
+dataset = load_dataset("dataset/qa_pairs.json", split = "train")
+dataset = dataset.map(formatting_prompts_func, batched = True,)
