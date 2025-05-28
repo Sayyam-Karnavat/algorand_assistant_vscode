8afdf7ced91db1a120810541a061679b445b42fe
Sayyam-Karnavat <sanyam.karnavat5@gmail.com>
2025-05-28 18:32:26 +0530

    Convert the model int GGUF format so that it can run locally (8 bit or 16 bit )

--- Fine_tune/ollama_finetuning.py
+++ Fine_tune/ollama_finetuning.py
@@ -126,3 +126,28 @@ if False: model.push_to_hub_merged("hf/model", tokenizer, save_method = "merged_
 # Just LoRA adapters
 if False: model.save_pretrained_merged("model", tokenizer, save_method = "lora",)
 if False: model.push_to_hub_merged("hf/model", tokenizer, save_method = "lora", token = "")
+
+
+
+# Save to 8bit Q8_0
+if False: model.save_pretrained_gguf("model", tokenizer,)
+# Remember to go to https://huggingface.co/settings/tokens for a token!
+
+if False: model.push_to_hub_gguf("hf/model", tokenizer, token = "")
+
+# Save to 16bit GGUF
+if False: model.save_pretrained_gguf("model", tokenizer, quantization_method = "f16")
+if False: model.push_to_hub_gguf("hf/model", tokenizer, quantization_method = "f16", token = "")
+
+# Save to q4_k_m GGUF
+if False: model.save_pretrained_gguf("model", tokenizer, quantization_method = "q4_k_m")
+if False: model.push_to_hub_gguf("hf/model", tokenizer, quantization_method = "q4_k_m", token = "")
+
+# Save to multiple GGUF options - much faster if you want multiple!
+if False:
+    model.push_to_hub_gguf(
+        "hf/model", # Change hf to your username!
+        tokenizer,
+        quantization_method = ["q4_k_m", "q8_0", "q5_k_m",],
+        token = "",
+    )
