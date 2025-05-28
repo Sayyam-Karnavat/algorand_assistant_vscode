ae2c58ac4a436f26c96456234f9315e067b1a474
Sayyam-Karnavat <sanyam.karnavat5@gmail.com>
2025-05-28 18:27:20 +0530

    Trainer class and arguments with tokenizer , dataset
    - Starting off with small batch size and accumulation steps for training.

--- Fine_tune/ollama_finetuning.py
+++ Fine_tune/ollama_finetuning.py
@@ -70,3 +70,34 @@ pass
 from datasets import load_dataset
 dataset = load_dataset("dataset/qa_pairs.json", split = "train")
 dataset = dataset.map(formatting_prompts_func, batched = True,)
+
+from trl import SFTTrainer
+from transformers import TrainingArguments
+from unsloth import is_bfloat16_supported
+
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = dataset,
+    dataset_text_field = "text",
+    max_seq_length = max_seq_length,
+    dataset_num_proc = 2,
+    packing = False, # Can make training 5x faster for short sequences.
+    args = TrainingArguments(
+        per_device_train_batch_size = 2,
+        gradient_accumulation_steps = 4,
+        warmup_steps = 5,
+        # num_train_epochs = 1, # Set this for 1 full training run.
+        max_steps = 60,
+        learning_rate = 2e-4,
+        fp16 = not is_bfloat16_supported(),
+        bf16 = is_bfloat16_supported(),
+        logging_steps = 1,
+        optim = "adamw_8bit",
+        weight_decay = 0.01,
+        lr_scheduler_type = "linear",
+        seed = 3407,
+        output_dir = "outputs",
+        report_to = "none", # Use this for WandB etc
+    ),
+)
