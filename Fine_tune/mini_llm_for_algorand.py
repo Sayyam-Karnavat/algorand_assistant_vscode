bf546689cf1946889d8eb255dda0d3ee209b2fb0
Sayyam-Karnavat <sanyam.karnavat5@gmail.com>
2025-05-28 18:38:45 +0530

    LSTM Neural network architecture added
    - Model compiled and training is started

--- Fine_tune/mini_llm_for_algorand.py
+++ Fine_tune/mini_llm_for_algorand.py
@@ -51,3 +51,34 @@ step_size = 3
 for i in range(0,len(text)-seq_length,step_size):
     sentences.append(text[i : i + seq_length])# This is our feature data
     next_characters.append(text[i+seq_length])#This is our target data (this statement stores next single character)
+
+
+
+x = np.zeros((len(sentences),seq_length,len(unique_characters)), dtype = bool ) # features data 
+y = np.zeros((len(sentences),len(unique_characters)) , dtype = bool)
+
+
+
+# This loop acts as kind of one-hot encoding of unique characters present in the sentence generated
+
+for i, sentence in enumerate(sentences):
+    for t,character in enumerate(sentence):
+        x[i,t, char_to_index[character]]= 1
+    y[i,char_to_index[next_characters[i]]] = 1 
+
+
+
+model = Sequential()
+model.add(LSTM(128,input_shape =(seq_length,len(unique_characters))))
+model.add(Dense(len(unique_characters)))
+model.add(Activation('softmax'))
+
+
+model.compile(loss = 'categorical_crossentropy' ,optimizer = RMSprop(learning_rate = 0.01))
+
+model.fit(x,y,batch_size = 256 ,epochs = 4)
+
+
+
+model.save('text_generator.model')
+
